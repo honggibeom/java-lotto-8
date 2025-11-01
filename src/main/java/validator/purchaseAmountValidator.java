a3bc29db1dc97fb08848a12b53393e8dead25543
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-01 23:40:55 +0900

    feat: 로또 구입 금액 입력 값 검증 구현

---
+++ src/main/java/validator/purchaseAmountValidator.java
@@ -0,0 +1,27 @@
+package validator;
+
+public class purchaseAmountValidator {
+    private static final Integer LottoPrice = 1000;
+
+    public static void validate(String PurchaseAmount) {
+        validateNumber(PurchaseAmount);
+        int parsedPurchaseAmount = Integer.parseInt(PurchaseAmount);
+        validateGreaterThanZero(parsedPurchaseAmount);
+        validateMultipleOfThousand(parsedPurchaseAmount);
+    }
+
+    private static void validateNumber(String PurchaseAmount) {
+        if (PurchaseAmount.matches("^[0-9]"))
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+    }
+
+    private static void validateGreaterThanZero(int purchaseAmount) {
+        if (purchaseAmount <= 0)
+            throw new IllegalArgumentException("구입한 로또 가격은 0보다 커야합니다.");
+    }
+
+    private static void validateMultipleOfThousand(int purchaseAmount) {
+        if (purchaseAmount % LottoPrice != 0)
+            throw new IllegalArgumentException("로또는 1000원단위입니다.");
+    }
+}
