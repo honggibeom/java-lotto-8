d7d517279bb6a19bea2222223d3c321968cd1541
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-01 19:26:39 +0900

    fix: 제어자 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,18 +3,17 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-    String askLottoPrice() {
+    public String askLottoPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    String askWinningNumbers(){
+    public String askWinningNumbers(){
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    String askBonusNumbers(){
+    public String askBonusNumbers(){
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
