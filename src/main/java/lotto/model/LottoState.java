280c6cc85764432d06e4459d32132c75a8dd8294
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-02 11:20:12 +0900

    refactor: 로또 번호 개수 추가

--- src/main/java/lotto/model/LottoState.java
+++ src/main/java/lotto/model/LottoState.java
@@ -4,8 +4,8 @@ public enum LottoState {
 
     MinNumber(1),
     MaxNumber(45),
-    price(1000);
-
+    price(1000),
+    numberCount(6);
     private final Integer LottoNumber;
 
     LottoState(Integer LottoNumber) {
@@ -14,5 +14,13 @@ public enum LottoState {
 
     public Integer getState(){
         return LottoNumber;
+    }
+
+    public int calculateLottoCountFromAmount(int amount){
+        return amount/price.getState();
+    }
+
+    public int calculateAmountFromLottoCount(int lottoCount){
+        return lottoCount*price.getState();
     }
 }
