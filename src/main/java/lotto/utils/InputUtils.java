34941679caba2f01a2d57c2cd6c7b9d41675ee18
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-02 11:13:19 +0900

    feat: 예외가 발생할 경우 입력을 다시 받는 기능 구현

---
+++ src/main/java/lotto/utils/InputUtils.java
@@ -0,0 +1,16 @@
+package lotto.utils;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+public class InputUtils {
+    public static String askAndValidate(Supplier<String> inputSupplier, Consumer<String> validator) {
+        while (true) {
+            try {
+                String input = inputSupplier.get();
+                validator.accept(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
