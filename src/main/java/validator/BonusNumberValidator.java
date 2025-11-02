1ca118b35e446521751f3504d787512b8617cb7e
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-02 11:12:19 +0900

    fix: 보너스 번호 범위 오류 수정

--- src/main/java/validator/BonusNumberValidator.java
+++ src/main/java/validator/BonusNumberValidator.java
@@ -15,7 +15,7 @@ public class BonusNumberValidator {
     }
 
     private static void validateBetweenMinNumberThanMaxNumber(int bonusNumber) {
-        if (bonusNumber < LottoState.MinNumber.getState() || bonusNumber >= LottoState.MaxNumber.getState())
+        if (bonusNumber < LottoState.MinNumber.getState() || bonusNumber > LottoState.MaxNumber.getState())
             throw new IllegalArgumentException("보너스 숫자는 1~45사이의 숫자를 입력해야합니다.");
     }
 }
