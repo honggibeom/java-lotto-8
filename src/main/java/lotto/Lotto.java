0b55c6fc12a44d6735212d3f3801e4cd3c4a0b75
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-11-02 11:18:26 +0900

    feat: 로또 별 당첨 등수 확인 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import lotto.model.WinningCondition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +21,28 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isMatchWithBonusNumber(Integer BonusNumber){
+        return numbers.contains(BonusNumber);
+    }
+
+    private int CountMatchNumber(Set<Integer>WinningNumbers){
+        return (int) numbers.stream()
+                .filter(WinningNumbers::contains)
+                .count();
+    }
+
+    public int getRanking(Set<Integer>WinningNumbers,Integer BonusNumber){
+        boolean matchBonusNumber = isMatchWithBonusNumber(BonusNumber);
+        int countMatchNumber = CountMatchNumber(WinningNumbers);
+        return Arrays.stream(WinningCondition.values()).filter(
+                 winningCondition->matchBonusNumber==winningCondition.getMatchBonusNumber()&&
+                         countMatchNumber==winningCondition.getMatchNumberCount())
+                 .findFirst()
+                 .map(rank -> rank.ordinal() + 1)
+                 .orElse(-1);
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
 }
